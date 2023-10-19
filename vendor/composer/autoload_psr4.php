463d8961c61479ae59f784ff6171af4d6e008406
Usman Shoukat <121452897+Usmanshoukat001@users.noreply.github.com>
2023-10-19 12:12:16 +0500

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Mirza\\PackageEc\\' => array($baseDir . '/src'),
+    'UsmanMirzashoukat\\PackageEc\\' => array($baseDir . '/src'),
 );
